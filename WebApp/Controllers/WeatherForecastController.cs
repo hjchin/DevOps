6a11bb3399452ada44f5a4682aae54f504423005
Hoong Jhang Chin <hoongjhang.chin@singtel.com>
2024-10-04 16:27:25 +0800

    update

--- WebApp/Controllers/WeatherForecastController.cs
+++ WebApp/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace DevOps.Controllers;
 
@@ -11,17 +12,21 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
+    private readonly IConfiguration Configuration;
     private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
     {
         _logger = logger;
+        Configuration = configuration;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+
+        int resultCount = int.Parse(Configuration["ResultCount"]);
+        return Enumerable.Range(1, resultCount).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
